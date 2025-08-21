d2f09614764e1ac94ca7cc1dc1b54d6d2ce9b8f4
LukasMirbt <lukas.mirbt@hotmail.com>
2025-08-21 14:14:40 +0200

    fix

--- lib/app_router/models/app_router.dart
+++ lib/app_router/models/app_router.dart
@@ -35,13 +35,8 @@ class AppRouter {
 
   String get currentLocation => goRouter.state.uri.toString();
 
-  String get from {
-    final state = goRouter.state;
-    final uri = state.uri;
-    final queryParameters = uri.queryParameters;
-    final from = queryParameters['from'];
-    return from ?? AppRouter.initialLocation;
-  }
+  String get from =>
+      goRouter.state.uri.queryParameters['from'] ?? AppRouter.initialLocation;
 
   void go(GoRouteData route) {
     final redirect = _redirectModel.redirect(
