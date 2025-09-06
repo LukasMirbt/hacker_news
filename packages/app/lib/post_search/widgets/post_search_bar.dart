72d0348254a1142aad335ce2858e13bdda263432
danielamaric <daniela.maric98@icloud.com>
2025-09-06 16:11:12 +0200

    fix

--- packages/app/lib/post_search/widgets/post_search_bar.dart
+++ packages/app/lib/post_search/widgets/post_search_bar.dart
@@ -2,35 +2,13 @@ import 'package:app/post_search/post_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PostSearchBar extends StatefulWidget {
+class PostSearchBar extends StatelessWidget {
   const PostSearchBar({super.key});
-
-  @override
-  State<PostSearchBar> createState() => _PostSearchBarState();
-}
-
-class _PostSearchBarState extends State<PostSearchBar> {
-  late FocusNode _focusNode;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode = FocusNode();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (mounted) _focusNode.requestFocus();
-    });
-  }
-
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return SearchBar(
-      focusNode: _focusNode,
+      autoFocus: true,
       backgroundColor: const WidgetStatePropertyAll(Colors.transparent),
       elevation: const WidgetStatePropertyAll(0),
       overlayColor: const WidgetStatePropertyAll(Colors.transparent),
