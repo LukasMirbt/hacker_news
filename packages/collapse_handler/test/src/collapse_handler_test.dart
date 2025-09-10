7ef0eace0ff3a33323184939993845f899dc1f5d
LukasMirbt <lukas.mirbt@hotmail.com>
2025-09-10 16:34:46 +0200

    collapse handler tests

--- packages/collapse_handler/test/src/collapse_handler_test.dart
+++ packages/collapse_handler/test/src/collapse_handler_test.dart
@@ -2,10 +2,11 @@
 // ignore_for_file: avoid_redundant_argument_values
 
 import 'package:collapse_handler/collapse_handler.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class _TestItem implements Collapsible<_TestItem> {
-  _TestItem({
+class _TestItem extends Equatable implements Collapsible<_TestItem> {
+  const _TestItem({
     required this.id,
     this.indent = 0,
     this.isExpanded = true,
@@ -38,32 +39,155 @@ class _TestItem implements Collapsible<_TestItem> {
       isParentExpanded: isParentExpanded ?? this.isParentExpanded,
     );
   }
+
+  @override
+  List<Object?> get props => [
+    id,
+    indent,
+    isExpanded,
+    isParentExpanded,
+  ];
 }
 
 void main() {
   group(CollapseHandler, () {
     CollapseHandler createSubject() => CollapseHandler<_TestItem>();
 
+    group('toggleExpansion', () {
+      test('correctly collapses a parent item '
+          'and hides all its children', () {
+        final items = [
+          _TestItem(id: '0', indent: 0),
+          _TestItem(id: '1', indent: 1),
+          _TestItem(id: '2', indent: 2),
+          _TestItem(id: '3', indent: 1),
+          _TestItem(id: '4', indent: 0),
+        ];
+        final handler = createSubject();
+        final updatedItems = handler.toggleExpansion(
+          items: items,
+          index: 0,
+        );
+        expect(
+          updatedItems,
+          [
+            items[0].copyWith(isExpanded: false),
+            items[1].copyWith(isParentExpanded: false),
+            items[2].copyWith(isParentExpanded: false),
+            items[3].copyWith(isParentExpanded: false),
+            items[4],
+          ],
+        );
+      });
+
+      test('correctly expands a collapsed parent item '
+          'and shows its children', () {
+        final items = [
+          _TestItem(id: '0', indent: 0, isExpanded: false),
+          _TestItem(id: '1', indent: 1, isParentExpanded: false),
+          _TestItem(id: '2', indent: 2, isParentExpanded: false),
+          _TestItem(id: '3', indent: 1, isParentExpanded: false),
+          _TestItem(id: '4', indent: 0),
+        ];
+        final handler = createSubject();
+        final updatedItems = handler.toggleExpansion(
+          items: items,
+          index: 0,
+        );
+        expect(updatedItems, [
+          items[0].copyWith(isExpanded: true),
+          items[1].copyWith(isParentExpanded: true),
+          items[2].copyWith(isParentExpanded: true),
+          items[3].copyWith(isParentExpanded: true),
+          items[4],
+        ]);
+      });
+
+      test('correctly expands a parent but keeps grandchildren hidden '
+          'when their direct parent is collapsed', () {
+        final items = [
+          _TestItem(id: '0', indent: 0, isExpanded: false),
+          _TestItem(
+            id: '1',
+            indent: 1,
+            isExpanded: false,
+            isParentExpanded: false,
+          ),
+          _TestItem(id: '2', indent: 2, isParentExpanded: false),
+          _TestItem(id: '3', indent: 1, isParentExpanded: false),
+          _TestItem(id: '4', indent: 0),
+        ];
+        final handler = createSubject();
+        final updatedItems = handler.toggleExpansion(
+          items: items,
+          index: 0,
+        );
+        expect(
+          updatedItems,
+          [
+            items[0].copyWith(isExpanded: true),
+            items[1].copyWith(isParentExpanded: true),
+            items[2].copyWith(isParentExpanded: false),
+            items[3].copyWith(isParentExpanded: true),
+            items[4],
+          ],
+        );
+      });
+    });
+
+    group('ensureVisible', () {
+      final items = [
+        _TestItem(id: '0', indent: 0),
+        _TestItem(id: '1', indent: 1, isExpanded: false),
+        _TestItem(id: '2', indent: 2, isParentExpanded: false),
+        _TestItem(id: '3', indent: 1),
+        _TestItem(id: '4', indent: 0),
+      ];
+
+      test('correctly expands all parent items '
+          'to make a nested item visible', () {
+        final handler = createSubject();
+        final updatedItems = handler.ensureVisible(
+          items: items,
+          index: 2,
+        );
+        expect(
+          updatedItems,
+          [
+            items[0],
+            items[1].copyWith(isExpanded: true),
+            items[2].copyWith(isParentExpanded: true),
+            items[3],
+            items[4],
+          ],
+        );
+      });
+
+      test('returns same items when item is already visible', () {
+        final handler = createSubject();
+        final updatedItems = handler.ensureVisible(
+          items: items,
+          index: 4,
+        );
+        expect(updatedItems, items);
+      });
+
+      test('handles the root item correctly', () {
+        final handler = createSubject();
+        final updatedItems = handler.ensureVisible(
+          items: items,
+          index: 0,
+        );
+        expect(updatedItems, items);
+      });
+    });
+
     group('rebuildWith', () {
-      final initialItems = [
-        _TestItem(
-          id: '0',
-          indent: 0,
-        ),
-        _TestItem(
-          id: '1',
-          indent: 1,
-          isExpanded: false,
-        ),
-        _TestItem(
-          id: '2',
-          indent: 2,
-          isParentExpanded: false,
-        ),
-        _TestItem(
-          id: '3',
-          indent: 0,
-        ),
+      final oldItems = [
+        _TestItem(id: '0', indent: 0),
+        _TestItem(id: '1', indent: 1, isExpanded: false),
+        _TestItem(id: '2', indent: 2, isParentExpanded: false),
+        _TestItem(id: '3', indent: 0),
       ];
 
       final newItems = [
@@ -74,39 +198,29 @@ void main() {
         _TestItem(id: '4', indent: 1),
       ];
 
-      test('preserves existing expansion states for old items', () {
-        final handler = createSubject();
-        final rebuiltList = handler.rebuildWith(
-          oldItems: initialItems,
-          newItems: newItems,
-        );
-        expect(rebuiltList[1].isExpanded, false);
-      });
-
-      test('sets isExpanded to true for new items', () {
+      test('correctly preserves old states, defaults new states, '
+          'and recalculates visibility', () {
         final handler = createSubject();
         final rebuiltItems = handler.rebuildWith(
-          oldItems: initialItems,
+          oldItems: oldItems,
           newItems: newItems,
         );
-        expect(rebuiltItems[4].isExpanded, true);
-      });
 
-      test('correctly recalculates isParentExpanded for all items', () {
-        final handler = createSubject();
-        final rebuiltItems = handler.rebuildWith(
-          oldItems: initialItems,
-          newItems: newItems,
+        expect(
+          rebuiltItems,
+          [
+            _TestItem(id: '0', indent: 0),
+            _TestItem(
+              id: '1',
+              indent: 1,
+              isExpanded: false,
+              isParentExpanded: true,
+            ),
+            _TestItem(id: '2', indent: 2, isParentExpanded: false),
+            _TestItem(id: '3', indent: 0),
+            _TestItem(id: '4', indent: 1),
+          ],
         );
-        expect(rebuiltItems[0].isExpanded, true);
-        expect(rebuiltItems[0].isParentExpanded, true);
-        expect(rebuiltItems[1].isExpanded, false);
-        expect(rebuiltItems[1].isParentExpanded, true);
-        expect(rebuiltItems[2].isParentExpanded, false);
-        expect(rebuiltItems[3].isExpanded, true);
-        expect(rebuiltItems[3].isParentExpanded, true);
-        expect(rebuiltItems[4].isExpanded, true);
-        expect(rebuiltItems[4].isParentExpanded, true);
       });
 
       test('handles an empty initial list', () {
@@ -117,81 +231,6 @@ void main() {
         );
         expect(rebuiltItems.length, 5);
         expect(rebuiltItems.every((item) => item.isExpanded), true);
-      });
-    });
-
-    group('toggleExpansion', () {
-      final nestedItems = [
-        _TestItem(id: '0', indent: 0),
-        _TestItem(id: '1', indent: 1),
-        _TestItem(id: '2', indent: 2),
-        _TestItem(id: '3', indent: 1),
-        _TestItem(id: '4', indent: 0),
-      ];
-
-      test('returns original items when item is not found', () {
-        final handler = createSubject();
-        final updatedItems = handler.toggleExpansion(
-          items: nestedItems,
-          itemToToggle: _TestItem(id: ''),
-        );
-        expect(updatedItems, nestedItems);
-      });
-
-      test('correctly collapses a parent item '
-          'and hides all its children', () {
-        final handler = createSubject();
-        final tappedItem = nestedItems[0];
-        final updatedItems = handler.toggleExpansion(
-          items: nestedItems,
-          itemToToggle: tappedItem,
-        );
-        expect(updatedItems[0].isExpanded, false);
-        expect(updatedItems[1].isParentExpanded, false);
-        expect(updatedItems[2].isParentExpanded, false);
-        expect(updatedItems[3].isParentExpanded, false);
-        expect(updatedItems[4].isParentExpanded, true);
-      });
-
-      test('correctly expands a collapsed parent item '
-          'and shows its children', () {
-        final initialItems = [
-          nestedItems[0].copyWith(isExpanded: false),
-          nestedItems[1].copyWith(isParentExpanded: false),
-          nestedItems[2].copyWith(isParentExpanded: false),
-          nestedItems[3].copyWith(isParentExpanded: false),
-          nestedItems[4],
-        ];
-        final handler = createSubject();
-        final tappedItem = initialItems[0];
-        final updatedItems = handler.toggleExpansion(
-          items: initialItems,
-          itemToToggle: tappedItem,
-        );
-        expect(updatedItems[0].isExpanded, true);
-        expect(updatedItems[1].isParentExpanded, true);
-        expect(updatedItems[2].isParentExpanded, true);
-        expect(updatedItems[3].isParentExpanded, true);
-      });
-
-      test('correctly expands a parent but keeps grandchildren hidden '
-          'when their direct parent is collapsed', () {
-        final initialItems = [
-          nestedItems[0].copyWith(isExpanded: false),
-          nestedItems[1].copyWith(isExpanded: false, isParentExpanded: false),
-          nestedItems[2].copyWith(isParentExpanded: false),
-          nestedItems[3].copyWith(isParentExpanded: false),
-          nestedItems[4],
-        ];
-        final handler = createSubject();
-        final tappedItem = initialItems[0];
-        final updatedItems = handler.toggleExpansion(
-          items: initialItems,
-          itemToToggle: tappedItem,
-        );
-        expect(updatedItems[0].isExpanded, true);
-        expect(updatedItems[1].isParentExpanded, true);
-        expect(updatedItems[2].isParentExpanded, false);
       });
     });
   });
