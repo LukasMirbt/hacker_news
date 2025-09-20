3a532953f88b5a5baee9dd0a3b0f57586fc77670
danielamaric <daniela.maric98@icloud.com>
2025-09-20 23:41:28 +0200

    no stutter

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:app_client_platform_configuration/app_client_platform_configurat
 import 'package:app_logging/app_logging.dart';
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,27 +24,12 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(
-    !kDebugMode,
-  );
-
   final logger = AppLogging.start(
     debug: kDebugMode,
     integrations: [
       const ConsoleLoggingIntegration(),
-      CrashlyticsLoggingIntegration(
-        firebaseApp,
-        firebaseCrashlytics: FirebaseCrashlytics.instance,
-      ),
     ],
   );
-
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
 
   final analyticsConsentStorage = AnalyticsConsentStorage(
     sharedPreferences: sharedPreferences,
